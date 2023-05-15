184b5e2e90e296ab661eeccb7d5647f4b2d4cee5
mhimranhossain <mh.imranhossain@outlook.com>
2023-05-15 10:49:33 +0600

    Update 05/15/2023 10:49:33

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export const error = () => {
+	return (
+		<div className="flex min-h-screen text-white bg-black">
+			<h1 className="text-9xl font-bold underline decoration-red-500">
+				Error
+			</h1>
+		</div>
+	)
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+// 'use client'
+
+export default function notfound() {
+	return (
+		<div className="flex min-h-screen justify-center items-center text-white bg-black">
+			<h1 className="text-9xl font-bold underline decoration-red-500">
+				Not Found
+			</h1>
+		</div>
+	)
+}
