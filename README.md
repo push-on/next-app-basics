a3342228ae264dabb9b0fb9e34a139dbabecfdb5
mhimranhossain <mh.imranhossain@outlook.com>
2023-05-17 12:51:25 +0600

    Update 05/17/2023 12:51:25

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Next JS Basics
+
+# issues
+
+- next/link not redirecting from not-found.tsx
+  - 👉 [github issue](https://github.com/vercel/next.js/issues/49277)
+  - []()
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,12 @@
-export default function page () {
+import Link from "next/link"
+
+export default function page() {
 	return (
-		<div>page</div>
+		<div className="flex min-h-screen justify-center items-center text-white bg-black">
+			<h1 className="text-6xl font-bold underline underline-offset-8 decoration-indigo-500 ">
+				Page
+			</h1>
+			<Link className="text-3xl duration-150 transition-all hover:text-orange-400 hover:underline decoration-yellow-400 underline-offset-8" href={'/'}>Route: / Home</Link>
+		</div>
 	)
 }
